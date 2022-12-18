8a9fe2f4245223d92da4ae1ef15ed5dadde3afb1
TheMajorN <nickbaranosky@gmail.com>
2022-12-18 14:06:12 -0500

    Added structure NBTs & modified file structure

--- src/main/java/com/themajorn/modulardungeons/ModularDungeons.java
+++ src/main/java/com/themajorn/modulardungeons/ModularDungeons.java
@@ -3,13 +3,7 @@ package com.themajorn.modulardungeons;
 import com.mojang.logging.LogUtils;
 import com.themajorn.modulardungeons.core.registry.ModEntities;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
@@ -20,16 +14,14 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 import software.bernie.geckolib.GeckoLib;
 
 @Mod(ModularDungeons.MODID)
 public class ModularDungeons
 {
-    public static final String MODID = "modulardungeons";
+    public static final String MODID = "assets/modulardungeons";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public ModularDungeons() {
--- src/main/java/com/themajorn/modulardungeons/common/entities/Goblin.java
+++ src/main/java/com/themajorn/modulardungeons/common/entities/Goblin.java
@@ -5,9 +5,12 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
 
 
-public class Goblin extends Monster implements IAnimatable{
+public class Goblin extends Monster implements GeoAnimatable {
 
     public Goblin(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
@@ -18,5 +21,20 @@ public class Goblin extends Monster implements IAnimatable{
                                                 .add(Attributes.FOLLOW_RANGE, 20)
                                                 .add(Attributes.MOVEMENT_SPEED, .27)
                                                 .add(Attributes.MAX_HEALTH, 14);
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return null;
+    }
+
+    @Override
+    public double getTick(Object object) {
+        return 0;
     }
 }
